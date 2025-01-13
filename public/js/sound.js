1b42b6c4a79653aded4a9aace89040d8e67cfa83
Monthira Chayabanjonglerd <acaideddesign3@gmail.com>
2025-01-13 15:54:51 +0000

    update image to webp

--- public/js/sound.js
+++ public/js/sound.js
@@ -6,11 +6,11 @@ audioElement.volume = 0.5;
 
 toggleSound.addEventListener("click", function () {
     if (audioElement.paused == false) {
-        toggleSound.src = "https://cdn.glitch.global/9737b693-af83-452d-a9df-854f39d63332/sound_off.png?v=1735541964445";
+        toggleSound.src = "https://cdn.glitch.global/9737b693-af83-452d-a9df-854f39d63332/sound_off.webp?v=1736783236059";
         toggleSound.alt ="sound off";
         audioElement.pause();
     } else {
-        toggleSound.src = "https://cdn.glitch.global/9737b693-af83-452d-a9df-854f39d63332/sound_on.png?v=1735541962522";
+        toggleSound.src = "https://cdn.glitch.global/9737b693-af83-452d-a9df-854f39d63332/sound_on.webp?v=1736783322993";
         toggleSound.alt ="sound on";
         audioElement.currentTime = 0;
         audioElement.play();
