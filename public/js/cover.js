52ebdbbefe86604a7f6b275bca76d2ff06330008
Monthira Chayabanjonglerd <acaideddesign3@gmail.com>
2025-01-14 15:01:27 +0000

    fix for failed with update value

--- public/js/cover.js
+++ public/js/cover.js
@@ -3,7 +3,7 @@ const moonWidth = moonElement.offsetWidth;
 
 const rocketElement = document.getElementById('rocket');
 const rocketWidth = rocketElement.offsetWidth;
-const maxTranslateX = window.innerWidth - rocketWidth - moonWidth;
+var maxTranslateX = window.innerWidth - rocketWidth - moonWidth;
 
 document.addEventListener('scroll', function () {
   const translateX = Math.min(window.scrollY * 2, maxTranslateX);
