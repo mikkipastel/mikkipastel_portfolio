8da2ea38113f89c191d01bdabb8e570a90fc3b19
Monthira Chayabanjonglerd <acaideddesign3@gmail.com>
2025-04-10 02:06:50 +0000

    get lasted blog from json local

--- public/js/fetchBlog.js
+++ public/js/fetchBlog.js
@@ -1,40 +1,37 @@
 async function fetchLastedBlog() {
     const parentView = document.getElementById("lasted-blog");
-    parentView.style.display = 'none';
-    try {
-        const key = process.env.GHOST_CMS_KEY;
-        const response = await fetch(
-            `https://www.mikkipastel.com/ghost/api/v3/content/posts/?key=${key}&fields=title,url,feature_image&limit=5`
-        );
-    
-        if (!response.ok) {
-            throw new Error("cannot get some data.");
-        }
-        const responseJSON = await response.json();
-        const blogList = responseJSON.posts;
-        console.log(blogList);
-    
-        const blogListElement = document.getElementById("blog-list");
-        blogList.forEach((blog) => {
-            const item = document.createElement("div");
-            item.className = "blog-item";
-            item.innerHTML = `<a href="${blog.url}" target="_blank">
-                <img src="${blog.feature_image}" alt="${blog.title}">
-                <p>${blog.title}</p>
-                </a>`;
-            blogListElement.appendChild(item);
-        });
-    
-        const more = document.createElement("div");
-        more.className = "blog-more";
-        more.innerHTML = `<a href="https://www.mikkipastel.com/" target="_blank">
-                <p>more >></p>
-                </a>`;
-        blogListElement.appendChild(more);
-        parentView.style.display = 'block';
-        } catch (error) {
-            console.error("Error fetching lasted blog list:", error);
-        }
-    }
-fetchLastedBlog();
+    parentView.style.display = "none";
   
+    try {
+      const file = "../json/blog.json";
+      fetch(file)
+        .then((response) => response.json())
+        .then((blogs) => {
+          // set data
+          const blogListElement = document.getElementById("blog-list");
+          console.log(blogs);
+        
+          blogs.posts.forEach((blog) => {
+              const item = document.createElement("div");
+              item.className = "blog-item";
+              item.innerHTML = `<a href="${blog.url}" target="_blank">
+                  <img src="${blog.feature_image}" alt="${blog.title}">
+                  <p>${blog.title}</p>
+                </a>`;
+              blogListElement.appendChild(item);
+            });
+          const more = document.createElement("div");
+          more.className = "blog-more";
+          more.innerHTML = `<a href="https://www.mikkipastel.com/" target="_blank">
+                  <p>more >></p>
+                </a>`;
+          blogListElement.appendChild(more);
+          parentView.style.display = "block";
+        })
+        .catch((error) => console.error("Error fetching lasted blog list:", error));
+    } catch (error) {
+      console.error("Error fetching lasted blog list:", error);
+    }
+  }
+  fetchLastedBlog();
+    
