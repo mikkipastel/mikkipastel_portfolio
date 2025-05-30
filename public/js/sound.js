83bfec0da26168352ee619d4d89b7b8b48d04de4
Monthira Chayabanjonglerd <acaideddesign3@gmail.com>
2025-05-30 15:28:10 +0000

    move static media to this project

--- public/js/sound.js
+++ public/js/sound.js
@@ -6,11 +6,11 @@ audioElement.volume = 0.5;
 
 toggleSound.addEventListener("click", function () {
     if (audioElement.paused == false) {
-        toggleSound.src = "https://cdn.glitch.global/9737b693-af83-452d-a9df-854f39d63332/sound_off.webp?v=1736783236059";
+        toggleSound.src = "image/portfolio/sound_off.webp";
         toggleSound.alt ="sound off";
         audioElement.pause();
     } else {
-        toggleSound.src = "https://cdn.glitch.global/9737b693-af83-452d-a9df-854f39d63332/sound_on.webp?v=1736783322993";
+        toggleSound.src = "image/portfolio/sound_on.webp";
         toggleSound.alt ="sound on";
         audioElement.currentTime = 0;
         audioElement.play();
